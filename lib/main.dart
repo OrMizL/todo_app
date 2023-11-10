d6964411fe4ed3d87d692da836f6080b19e72cc0
Or Mizrahi <or0mizrahi@gmail.com>
2023-11-10 19:29:54 +0200

    final first version

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(TodoApp());
 }
 
-class MyApp extends StatelessWidget {
+class TodoApp extends StatelessWidget {
+  const TodoApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,56 +14,102 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: TodoList(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class TodoList extends StatefulWidget {
+  const TodoList({super.key});
+
+  @override
+  State<TodoList> createState() => _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
+  final List<Todo> _todos = <Todo>[];
+  final TextEditingController _textFieldController = TextEditingController();
+
+  void _addTodoItem(String name) {
+    setState(() {
+      _todos.add(Todo(name: name, completed: false));
+    });
+    _textFieldController.clear();
+  }
+
+  void _handleTodoChange(Todo todo) {
+    setState(() {
+      todo.completed = !todo.completed;
+    });
+  }
+
+  void _handleTodoDelete(Todo todo) {
+    setState(() {
+      _todos.removeWhere((element) => element.name == todo.name);
+    });
+  }
+
+  Future<void> _displayDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Add a Todo'),
+          content: TextField(
+            controller: _textFieldController,
+            decoration: const InputDecoration(hintText: 'Type your todo'),
+            autofocus: true,
+          ),
+          actions: <Widget>[
+            OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancel'),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+                _addTodoItem(_textFieldController.text);
+              },
+              child: const Text('Add'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('TODO'),
       ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text(
-                'Drawer Header',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                ),
-              ),
-            ),
-            ListTile(
-              leading: const Icon(Icons.home),
-              title: const Text('Home'),
-              onTap: () {
-                // Add your navigation logic here
-              },
-            ),
-            // Add more ListTiles for additional options in the drawer
-          ],
-        ),
-      ),
-      body: const Center(
-        child: Text(
-          'Welcome to your Flutter app!',
-          style: TextStyle(fontSize: 24),
-        ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(vertical: 10.0),
+        children: _todos.map((Todo todo) {
+          return TodoItem(
+            todo: todo,
+            onTodoChange: _handleTodoChange,
+            onTodoDelete: _handleTodoDelete
+          );
+        }).toList(),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Add your FAB action here
-        },
-        tooltip: 'Increment',
+        onPressed: () => _displayDialog(),
+        tooltip: 'Add Todo',
         shape: const BeveledRectangleBorder(
           borderRadius: BorderRadius.all(
             Radius.circular(16.0), // You can adjust the radius value as needed
@@ -72,4 +120,63 @@ class MyHomePage extends StatelessWidget {
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat, // Centering the FAB
     );
   }
+}
+
+class TodoItem extends StatelessWidget {
+  TodoItem({required this.todo, required this.onTodoChange, required this.onTodoDelete}) : super(key: ObjectKey(todo));
+
+  final Todo todo;
+  final void Function(Todo todo) onTodoChange;
+  final void Function(Todo todo) onTodoDelete;
+
+  TextStyle? _getTextStyle(bool checked) {
+    if (!checked) return null;
+
+    return const TextStyle(
+      color: Colors.black54,
+      decoration: TextDecoration.lineThrough,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: () {
+        onTodoChange(todo);
+      },
+      leading: Checkbox(
+        checkColor: Colors.greenAccent,
+        activeColor: Colors.red,
+        value: todo.completed,
+        onChanged: (value) {
+          onTodoChange(todo);
+        },
+      ),
+      title: Row(children: <Widget>[
+        Expanded(
+          child: Text(todo.name, style: _getTextStyle(todo.completed)),
+        ),
+        IconButton(
+          iconSize: 30,
+          icon: Icon(
+            Icons.delete,
+            color: Colors.red[300],
+          ),
+          alignment: Alignment.centerRight,
+          onPressed: () {
+            onTodoDelete(todo);
+          },
+        ),
+      ]),
+    );
+  }
+}
+
+class Todo {
+  Todo({
+    required this.name,
+    required this.completed
+  });
+  String name;
+  bool completed;
 }
