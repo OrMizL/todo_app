8aeb83d825853c124e3bead614da292d94f46066
Or Mizrahi <or0mizrahi@gmail.com>
2023-12-02 22:03:02 +0200

    styling upgrades, added a drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(TodoApp());
+  runApp(const TodoApp());
 }
 
 class TodoApp extends StatelessWidget {
@@ -14,7 +14,7 @@ class TodoApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: TodoList(),
+      home: const TodoList(),
     );
   }
 }
@@ -58,8 +58,14 @@ class _TodoListState extends State<TodoList> {
           title: const Text('Add a Todo'),
           content: TextField(
             controller: _textFieldController,
-            decoration: const InputDecoration(hintText: 'Type your todo'),
+            decoration: const InputDecoration(
+              hintText: 'Type your todo',
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.teal),
+              ),
+            ),
             autofocus: true,
+            cursorColor: Colors.teal,
           ),
           actions: <Widget>[
             OutlinedButton(
@@ -67,6 +73,8 @@ class _TodoListState extends State<TodoList> {
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(12),
                 ),
+                backgroundColor: Colors.teal,
+                foregroundColor: Colors.white,
               ),
               onPressed: () {
                 Navigator.of(context).pop();
@@ -78,6 +86,8 @@ class _TodoListState extends State<TodoList> {
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(12),
                 ),
+                backgroundColor: Colors.teal,
+                foregroundColor: Colors.white,
               ),
               onPressed: () {
                 Navigator.of(context).pop();
@@ -93,37 +103,88 @@ class _TodoListState extends State<TodoList> {
 
   @override
   Widget build(BuildContext context) {
+    double statusBarHeight = MediaQuery.of(context).padding.top;
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('TODO'),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Builder(
+          builder: (context) => IconButton(
+              onPressed: () => Scaffold.of(context).openDrawer(),
+              icon: const Icon(Icons.menu)),
+        ),
+        title: const Text('Your Todos'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.teal,
+        child: ListView(
+          // padding: EdgeInsets.only(top: 100),
+          children: [
+            SizedBox(
+              height: statusBarHeight,
+              child: const DrawerHeader(
+                child: Center(
+                  child: Text(
+                    '',
+                  ),
+                ),
+              ),
+            ),
+            const ListTile(
+              title: Text(
+                'First',
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
+            ),
+            const ListTile(
+              title: Text(
+                'Second',
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
+            )
+          ],
+        ),
       ),
       body: ListView(
         padding: const EdgeInsets.symmetric(vertical: 10.0),
         children: _todos.map((Todo todo) {
           return TodoItem(
-            todo: todo,
-            onTodoChange: _handleTodoChange,
-            onTodoDelete: _handleTodoDelete
-          );
+              todo: todo,
+              onTodoChange: _handleTodoChange,
+              onTodoDelete: _handleTodoDelete);
         }).toList(),
       ),
+      backgroundColor: Colors.teal,
       floatingActionButton: FloatingActionButton(
         onPressed: () => _displayDialog(),
         tooltip: 'Add Todo',
+        backgroundColor: Colors.white,
         shape: const BeveledRectangleBorder(
           borderRadius: BorderRadius.all(
             Radius.circular(16.0), // You can adjust the radius value as needed
           ),
         ),
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.add, color: Colors.teal),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat, // Centering the FAB
+      floatingActionButtonLocation:
+          FloatingActionButtonLocation.centerFloat, // Centering the FAB
     );
   }
 }
 
 class TodoItem extends StatelessWidget {
-  TodoItem({required this.todo, required this.onTodoChange, required this.onTodoDelete}) : super(key: ObjectKey(todo));
+  TodoItem(
+      {required this.todo,
+      required this.onTodoChange,
+      required this.onTodoDelete})
+      : super(key: ObjectKey(todo));
 
   final Todo todo;
   final void Function(Todo todo) onTodoChange;
@@ -173,10 +234,7 @@ class TodoItem extends StatelessWidget {
 }
 
 class Todo {
-  Todo({
-    required this.name,
-    required this.completed
-  });
+  Todo({required this.name, required this.completed});
   String name;
   bool completed;
 }
