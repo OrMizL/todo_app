e1e803394062d50eba50fcf95a27e8e73cc54149
Or Mizrahi <or0mizrahi@gmail.com>
2023-12-15 13:20:05 +0200

    organized files/folders, separated main file to widgets and models

---
+++ lib/src/screens/main_screen.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/src/models/todo.dart';
+import 'package:todo_app/src/widgets/todo_item.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final List<Todo> _todos = <Todo>[];
+  final TextEditingController _textFieldController = TextEditingController();
+
+  void _addTodoItem(String name) {
+    setState(() {
+      _todos.add(Todo(name: name, completed: false));
+    });
+    _textFieldController.clear();
+  }
+
+  void _handleTodoChange(Todo todo) {
+    setState(() {
+      todo.completed = !todo.completed;
+    });
+  }
+
+  void _handleTodoDelete(Todo todo) {
+    setState(() {
+      _todos.removeWhere((element) => element.name == todo.name);
+    });
+  }
+
+  Future<void> _displayDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Add a Todo'),
+          content: TextField(
+            controller: _textFieldController,
+            decoration: const InputDecoration(
+              hintText: 'Type your todo',
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.teal),
+              ),
+            ),
+            autofocus: true,
+            cursorColor: Colors.teal,
+          ),
+          actions: <Widget>[
+            OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                backgroundColor: Colors.teal,
+                foregroundColor: Colors.white,
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancel'),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                backgroundColor: Colors.teal,
+                foregroundColor: Colors.white,
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+                _addTodoItem(_textFieldController.text);
+              },
+              child: const Text('Add'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double statusBarHeight = MediaQuery.of(context).padding.top;
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Builder(
+          builder: (context) => IconButton(
+              onPressed: () => Scaffold.of(context).openDrawer(),
+              icon: const Icon(Icons.menu)),
+        ),
+        title: const Text('Your Todos'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.teal,
+        child: ListView(
+          // padding: EdgeInsets.only(top: 100),
+          children: [
+            SizedBox(
+              height: statusBarHeight,
+              child: const DrawerHeader(
+                child: Center(
+                  child: Text(
+                    '',
+                  ),
+                ),
+              ),
+            ),
+            const ListTile(
+              title: Text(
+                'First',
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
+            ),
+            const ListTile(
+              title: Text(
+                'Second',
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
+            )
+          ],
+        ),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(vertical: 10.0),
+        children: _todos.map((Todo todo) {
+          return TodoItem(
+              todo: todo,
+              onTodoChange: _handleTodoChange,
+              onTodoDelete: _handleTodoDelete);
+        }).toList(),
+      ),
+      backgroundColor: Colors.teal,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _displayDialog(),
+        tooltip: 'Add Todo',
+        backgroundColor: Colors.white,
+        shape: const BeveledRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(16.0), // You can adjust the radius value as needed
+          ),
+        ),
+        child: const Icon(Icons.add, color: Colors.teal),
+      ),
+      floatingActionButtonLocation:
+          FloatingActionButtonLocation.centerFloat, // Centering the FAB
+    );
+  }
+}
---
+++ lib/src/widgets/todo_item.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/src/models/todo.dart';
+
+class TodoItem extends StatelessWidget {
+  TodoItem(
+      {required this.todo,
+      required this.onTodoChange,
+      required this.onTodoDelete})
+      : super(key: ObjectKey(todo));
+
+  final Todo todo;
+  final void Function(Todo todo) onTodoChange;
+  final void Function(Todo todo) onTodoDelete;
+
+  TextStyle? _getTextStyle(bool checked) {
+    if (!checked) return null;
+
+    return const TextStyle(
+      color: Colors.black54,
+      decoration: TextDecoration.lineThrough,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: () {
+        onTodoChange(todo);
+      },
+      leading: Checkbox(
+        checkColor: Colors.greenAccent,
+        activeColor: Colors.red,
+        value: todo.completed,
+        onChanged: (value) {
+          onTodoChange(todo);
+        },
+      ),
+      title: Row(children: <Widget>[
+        Expanded(
+          child: Text(todo.name, style: _getTextStyle(todo.completed)),
+        ),
+        IconButton(
+          iconSize: 30,
+          icon: Icon(
+            Icons.delete,
+            color: Colors.red[300],
+          ),
+          alignment: Alignment.centerRight,
+          onPressed: () {
+            onTodoDelete(todo);
+          },
+        ),
+      ]),
+    );
+  }
+}
